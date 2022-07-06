4db8f41b6520e685d3451b0c0e1aefc49cd46947
yuji-inoguchi <52226280+yuji-inoguchi@users.noreply.github.com>
2022-07-06 23:14:47 +0900

    Responsive addition

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,5 +1,6 @@
-import { Box, Flex, Heading, Link } from "@chakra-ui/react";
+import { Box, Flex, Heading, IconButton, Link } from "@chakra-ui/react";
 import { memo, VFC } from "react";
+import { HamburgerIcon } from "@chakra-ui/icons";
 
 export const Header: VFC = memo(() => {
   return (
@@ -11,15 +12,29 @@ export const Header: VFC = memo(() => {
       justify="space-between"
       padding={{ base: 3, md: 5 }}
     >
-      <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
-        ユーザー管理アプリ
-      </Heading>
-      <Flex align="center" fontSize="sm">
+      <Flex aline="center" as="a" me={8} _hover={{ cursor: "pointer" }}>
+        <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
+          ユーザー管理アプリ
+        </Heading>
+      </Flex>
+      <Flex
+        align="center"
+        fontSize="sm"
+        flexGrow={2}
+        display={{ base: "none", md: "flex" }}
+      >
         <Box pr={4}>
           <Link>ユーザー一覧</Link>
         </Box>
         <Link>設定</Link>
       </Flex>
+      <IconButton
+        aria-label="メニューボタン"
+        icon={<HamburgerIcon />}
+        size="sm"
+        variant="unstyled"
+        display={{ base: "block", md: "none" }}
+      />
     </Flex>
   );
 });
