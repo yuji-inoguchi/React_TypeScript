02d99be1299267f61a6d9a447a675103819dabe7
yuji-inoguchi <52226280+yuji-inoguchi@users.noreply.github.com>
2022-07-11 23:31:26 +0900

    login_add

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,40 +1,64 @@
-import { Box, Flex, Heading, IconButton, Link } from "@chakra-ui/react";
-import { memo, VFC } from "react";
-import { HamburgerIcon } from "@chakra-ui/icons";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { Box, Flex, Heading, Link, useDisclosure } from "@chakra-ui/react";
+import { memo, useCallback, VFC } from "react";
+import { useHistory } from "react-router-dom";
+
+import { MenuIconButton } from "../../atoms/button/MenuIconButton";
+import { MenuDrawer } from "../../molecules/MenuDrawer";
+import { UserManagement } from "../../pages/UserManagement";
 
 export const Header: VFC = memo(() => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const history = useHistory();
+
+  const onClickHome = useCallback(() => history.push("/home"), []);
+  const onClickUserManagement = useCallback(
+    () => history.push("/home/user_management"),
+    []
+  );
+  const onClickSetting = useCallback(() => history.push("/home/setting"), []);
+
   return (
-    <Flex
-      as="nav"
-      bg="teal.500"
-      color="gray.50"
-      align="center"
-      justify="space-between"
-      padding={{ base: 3, md: 5 }}
-    >
-      <Flex aline="center" as="a" me={8} _hover={{ cursor: "pointer" }}>
-        <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
-          ユーザー管理アプリ
-        </Heading>
-      </Flex>
+    <>
       <Flex
+        as="nav"
+        bg="teal.500"
+        color="gray.50"
         align="center"
-        fontSize="sm"
-        flexGrow={2}
-        display={{ base: "none", md: "flex" }}
+        justify="space-between"
+        padding={{ base: 3, md: 5 }}
       >
-        <Box pr={4}>
-          <Link>ユーザー一覧</Link>
-        </Box>
-        <Link>設定</Link>
+        <Flex
+          aline="center"
+          as="a"
+          me={8}
+          _hover={{ cursor: "pointer" }}
+          onClick={onClickHome}
+        >
+          <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
+            ユーザー管理アプリ
+          </Heading>
+        </Flex>
+        <Flex
+          align="center"
+          fontSize="sm"
+          flexGrow={2}
+          display={{ base: "none", md: "flex" }}
+        >
+          <Box pr={4}>
+            <Link onClick={onClickUserManagement}>ユーザー一覧</Link>
+          </Box>
+          <Link onClick={onClickSetting}>設定</Link>
+        </Flex>
+        <MenuIconButton onOpen={onOpen} />
       </Flex>
-      <IconButton
-        aria-label="メニューボタン"
-        icon={<HamburgerIcon />}
-        size="sm"
-        variant="unstyled"
-        display={{ base: "block", md: "none" }}
+      <MenuDrawer
+        onClose={onClose}
+        isOpen={isOpen}
+        onClickHome={onClickHome}
+        onClickUserManagement={onClickUserManagement}
+        onClickSetting={onClickSetting}
       />
-    </Flex>
+    </>
   );
 });
---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { useCallback, useState } from "react";
+import { useHistory } from "react-router-dom";
+
+import { User } from "../types/api/user";
+import { useMessage } from "./useMessage";
+
+export const useAuth = () => {
+  const history = useHistory();
+  const { showMessage } = useMessage();
+
+  const [loading, setLoading] = useState(false);
+
+  const login = useCallback(
+    (id: string) => {
+      setLoading(true);
+
+      axios
+        .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
+        .then((res) => {
+          if (res.data) {
+            showMessage({ title: "ログインしました", status: "success" });
+            history.push("/home");
+          } else {
+            showMessage({ title: "ユーザーが見つかりません", status: "error" });
+          }
+        })
+        .catch(() =>
+          showMessage({ title: "ログインできません", status: "error" })
+        )
+        .finally(() => setLoading(false));
+    },
+    [history, showMessage]
+  );
+  return { login, loading };
+};
